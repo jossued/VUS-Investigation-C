e1666a59a36ee0b3e435c3ce75b4f83217aa9765
jossued <jossuedutan@gmail.com>
2019-04-11 13:05:00 -0500

    Obteniendo la energia por cada frame de 480 samples

--- energy_amplitude.c
+++ energy_amplitude.c
@@ -10,15 +10,20 @@
 #include <time.h>
 
 #define ELEMENTS 354240
+#define FRAMES 738
 #define FICHERO "o1_201704121431.dat"
-#define RESULTADOS "energy_amplitude.txt"
+#define RESULTADOS "energy_amplitud.dat"
 //#define numThreads 2
 
-typedef struct {
+typedef struct _amplitudes {
     char tiempo[20];
     char amplitud[20];
-    double energia;
+    double amplitud_cuad;
 } amplitudes;
+
+typedef struct _frames {
+    double energia;
+} FramesStruct;
 
 
 void leerDatos(amplitudes *R) {
@@ -58,119 +63,78 @@ void leerDatos(amplitudes *R) {
 
 }
 
-void calcularEnergias (amplitudes *R){
+void calcularCuad(amplitudes *R) {
 
     double test;
-    int i=0;
-    for (int j = 0; j < ELEMENTS*2; ++j) {
+    int i = 0;
+    for (int j = 0; j < ELEMENTS * 2; ++j) {
 //        printf("%s", R[j].amplitud);
 //        test = strtod(R[j].amplitud, NULL);
-        if (j % 2 != 0){
+        if (j % 2 != 0) {
             sscanf(R[j].amplitud, "%lf", &test);
-            R[i].energia=pow(test,2);
+            R[i].amplitud_cuad = pow(test, 2);
 //            printf("%.10lf\n", R[i].energia);
             i++;
         }
     }
 }
 
-/*
-int calcularDistancias(moleculas *R){
-
-    int bothAB = 0;
-    int onlyA = 0;
-    int onlyB = 0;
-
-    int *a;
-    int *b;
-
-    FILE *fp;
-    fp = fopen(RESULTADOS, "w");
-    int x = 1;
-    int *binA = malloc(4);
-    int *binB = malloc(4);
-
-    int tid;
-    int i=0;
-    int j=0;
-    int k=0;
-    int m=0;
-    //omp_set_num_threads(2);
-    for (int j = 0; j < ELEMENTS; ++j) {
-        for (int i = j + 1; i < ELEMENTS; ++i) {
-
-                for (k = 0; k < 512; ++k) {
-                    //printf("%c , %c\n",*((REGISTROS+j)->valor +k),*((REGISTROS+i)->valor +k));
-                    b = hex_bin(*(R[i].valor + k), &binA[0]);
-                    a = hex_bin(*(R[j].valor + k), &binB[0]);
-
-                    for (int m = 0; m < 4; ++m) { //   *(a+j)
-                        //printf("%d , %d\n",*(a+j),*(b+j));
-                        if (*(a + m) && *(b + m)) {
-                        	//#pragma critical
-                            bothAB++;
-                        }
-                        if (*(a + m) && *(b + m) == 0) {
-                            //#pragma critical
-                            onlyA++;
-                        }
-                        if (*(b + m) && *(a + m) == 0) {
-                            //#pragma critical
-                            onlyB++;
-                        }
-                    }
-
-                }
-
-
-
-            double sim = (double) bothAB / (onlyA + onlyB + bothAB);
-            double dist = 1 - sim;
-
-            fprintf(fp, "%d  %s,%s   Distancia: %.10f\n", x, R[j].id, R[i].id, dist);
-            bothAB = 0;
-            onlyA = 0;
-            onlyB = 0;
-            x++;
-
+void escribirStruct(amplitudes *R) {
+    FILE *outfile = fopen(RESULTADOS, "w");
+    char src[100];
+    if (outfile == NULL) {
+        fprintf(stderr, "\nError abriendo archivo\n");
+        exit(1);
+    } else {
+        // write struct to file
+        for (int i = 0; i < 2 * ELEMENTS; ++i) {
+//            strcpy(src,R[i]);
+//            fwrite(src, sizeof(amplitudes), 1, outfile);
+//            printf("%s\n", R[i]);
         }
 
+        if (fwrite != 0)
+            printf("contents to file written successfully !\n");
+        else
+            printf("error writing file !\n");
+
+        // close file
+        fclose(outfile);
     }
-
-
-    fclose(fp);
-    return x;
 }
 
-*/
+void calcularEnergia(amplitudes *RA, FramesStruct *RF) {
+    double sum = 0.0;
+    int j = 0;
+
+    for (int i = 0; i < ELEMENTS; i = i + 480) {
+//        printf("%lf\n", RA[i].amplitud_cuad);
+        for (int k = i; k < i + 480; ++k) {
+            sum += RA[k].amplitud_cuad;
+        }
+        RF[j].energia = sum;
+//        printf("%d Suma %lf\n", i, sum);
+        printf("%d Energia %.10lf\n", j, RF[j].energia);
+//        int c = getchar();
+
+        j++;
+
+        sum = 0.00;
+
+    }
+}
+
 
 int main(void) {
 
     //amplitudes REGISTROS[ELEMENTS];
     amplitudes *REGISTROS = malloc(2 * ELEMENTS * sizeof(amplitudes));
     leerDatos(REGISTROS);
-    calcularEnergias(REGISTROS);
+    calcularCuad(REGISTROS);
+    //escribirStruct(REGISTROS);
 
-
-    FILE *outfile;
-
-    // open file for writing
-    outfile = fopen (RESULTADOS, "w");
-    if (outfile == NULL)
-    {
-        fprintf(stderr, "\nError abriendo archivo\n");
-        exit (1);
-    }
-    // write struct to file
-    fwrite (&REGISTROS, sizeof(amplitudes), 1, outfile);
-
-    if(fwrite != 0)
-        printf("contents to file written successfully !\n");
-    else
-        printf("error writing file !\n");
-
-    // close file
-    fclose (outfile);
+    FramesStruct RFRAMES[FRAMES];
+    calcularEnergia(REGISTROS, RFRAMES);
 
     /*
     clock_t begin = clock();
